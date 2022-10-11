6f795254ae1bede3dfecb69ab08cd2b03f7c4a8e
Eugene Bravov <eugene.bravov@gmail.com>
2022-10-11 18:55:41 +0300

    add sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {LoginComponent} from './login/login.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    SidebarComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
