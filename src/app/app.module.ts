ce3734a8f558192d1242fc18ee706d76db46a936
Eugene Bravov <eugene.bravov@gmail.com>
2022-10-12 20:28:29 +0300

    Revert "add sidebar"
    This reverts commit 6f795254ae1bede3dfecb69ab08cd2b03f7c4a8e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {LoginComponent} from './login/login.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    LoginComponent,
-    SidebarComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  declarations: [AppComponent, PageNotFoundComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
